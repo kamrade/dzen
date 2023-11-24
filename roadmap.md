ab54691a2e5dccc4d05498021a465a2764817db3
Dennis <denys.mykhailov@zettahost.com>
2023-11-24 14:27:38 +0200

    Initial

---
+++ roadmap.md
@@ -0,0 +1,5 @@
+# Log and roadmap
+
+- [x] Install and adapt Sass
+- [x] Update vite configuration to enable absolute paths
+- [x] Install and adapt Bootstrap
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import s from './App.module.scss';
+
+function App() {
+  return (
+    <div className={s.App}>
+      <div className="container">
+        Dzen 1
+      </div>
+    </div>
+  )
+}
+
+export default App
