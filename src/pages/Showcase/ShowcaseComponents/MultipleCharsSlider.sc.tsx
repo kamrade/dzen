cfb007e5de5f4d845fb5f5b295ece79dd0b205d0
Dennis Michailov <denis.michailov@gmail.com>
2024-07-20 09:54:22 +0300

    Rearrange the Showcase

--- src/pages/Showcase/ShowcaseComponents/MultipleCharsSlider.sc.tsx
+++ src/pages/Showcase/ShowcaseComponents/MultipleCharsSlider.sc.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { CharsSlider } from '~/uikit';
-import s from './MultipleCharsSliderShowcase.module.scss';
 
 export const MultipleCharsSliderShowcase = () => {
   
@@ -15,7 +14,7 @@ export const MultipleCharsSliderShowcase = () => {
   }
 
   return (
-    <div className={`${s.MultipleCharsSliderShowcase} mb-3 d-flex`} onClick={clickHandler}>
+    <div className={`mono mb-3 d-flex`} onClick={clickHandler}>
       <CharsSlider 
         charFrom={charA} 
         charTo={charB}
